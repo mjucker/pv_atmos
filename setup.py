bbbb14cb1085b028f29a6d2bcb098df184e374fc
pviz <mjucker@mekon.princeton.edu>
2014-05-27 14:15:37 -0400

    Installable package
    pv-atmos can now be installed using setup.py and imported as package
    pv-atmos

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='pv_atmos',
+      version='1.0',
+      description='Utilities for scientific visualization with ParaView',
+      authot='Martin Jucker',
+      url='https://github.com/mjucker/pv_atmos',
+      package_dir={'pv_atmos': ''},
+      packages=['pv_atmos'],
+      )

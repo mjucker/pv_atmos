1eeae377ec933263388ae76db303df63270c5765
pv-atmos <mjucker@mjj.local>
2014-11-24 11:53:45 +0100

    New version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='pv_atmos',
-      version='2.0.1',
+      version='2.0.2',
       description='Utilities for scientific visualization with ParaView',
       long_description='Adds convenient functionality to visualize netCDF data in arbitrary coordinates, including logarithmic and spherical geometries. Mainly created for atmospheric and oceanic sciences in mind, it is general enough to be useful for many other applications. Read more on the GitHub page, or in the peer-reviewed open access article, which can be found at http://dx.doi.org/10.5334/jors.al',
       author='Martin Jucker',
